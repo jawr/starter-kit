117a7147b13d02cd243212b734f5a829b1174883
jawr <jawr@jarrah>
2013-07-09 23:22:45 +0100

    My changes..

--- js/app.js
+++ js/app.js
@@ -1,11 +1,56 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+/* models */
+App.Store = DS.Store.extend({
+  revision: 13,
+  adapter: DS.FixtureAdapter.create()
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
+App.Client = DS.Model.extend({
+  name: DS.attr('string'),
+  code: DS.attr('string')
+});
+
+/* routes */
+App.Router.map(function() {
+  this.route('index');
+  this.route('clients');  
+});
+
+App.ClientsRoute = Ember.Route.extend({
+  model: function() { return App.Client.find(); }
+});
+
+/* controllers */
+App.ClientsController = Ember.ArrayController.extend({
+  createClient: function() {
+
+    console.log(this);
+
+    var name = this.get('newName');
+    var code = this.get('newCode');
+
+    console.log(name);
+    console.log(code);
+
+    // create model
+    var client = App.Client.createRecord({
+      name: name,
+      code: code
+    });
+
+    // validate?
+    
+    // clean up
+    this.set('newName', '');
+    this.set('newCode', '');
+
+    client.save()
   }
 });
+
+/* fixtures */
+App.Client.FIXTURES = [
+  {id: 1, name: 'Test Client 1', code: 'AAO001'},
+  {id: 2, name: 'Fosters & Ting', code: 'AAO002'},
+];
